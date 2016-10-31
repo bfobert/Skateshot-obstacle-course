ef4546e12e197d80f4af589adc39c3d6e4c240f9
Patrick Doran <patrickdoran@patricks-mbp-6.elon.edu>
2016-10-31 19:26:41 -0400

    Accel

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class FPSInput : MonoBehaviour {
-	public float speed = 25.0f;
+	public float maxSpeed = 22.0f;
+	public float acceleration = 0f;
 	public float gravity = -9.8f;
 
 	private CharacterController charController;
@@ -12,13 +13,19 @@ public class FPSInput : MonoBehaviour {
 	}
 	//Controls speed of the player as well as gravity
 	void Update() {
-		float deltaX = Input.GetAxis("Horizontal") * speed;
-		float deltaZ = Input.GetAxis("Vertical") * speed;
+		float deltaX = Input.GetAxis("Horizontal") * acceleration;
+		float deltaZ = Input.GetAxis("Vertical") * acceleration;
 		Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-		movement = Vector3.ClampMagnitude(movement, speed);
+		movement = Vector3.ClampMagnitude(movement, acceleration);
 		movement.y = gravity;
 		movement *= Time.deltaTime;
 		movement = transform.TransformDirection(movement);
 		charController.Move(movement);
+		if (acceleration < maxSpeed) {
+			acceleration += .2f;
+		}
+		if (Input.GetAxis ("Horizontal") == 0 && Input.GetAxis ("Vertical") == 0) {
+			acceleration -= .4f;
+		}
 	}
 }
