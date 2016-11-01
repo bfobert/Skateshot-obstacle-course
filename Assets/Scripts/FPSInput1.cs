c7e2ae1e6310b174ea883c1cb5f5e6f917385703
Benjamin Fobert <bfobert@carpenter-09.elon.edu>
2016-11-01 14:12:16 -0400

    jump fixed

--- Assets/Scripts/FPSInput1.cs
+++ Assets/Scripts/FPSInput1.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FPSInput : MonoBehaviour {
+public class FPSInput1 : MonoBehaviour {
 	public float maxSpeed;
 	public float acceleration;
 	public float gravity;
@@ -13,42 +13,35 @@ public class FPSInput : MonoBehaviour {
 	void Start() {
 		charController = GetComponent<CharacterController>();
 	}
-
+	//Controls speed of the player as well as gravity
 	void Update() {
-		
-		//Jumps if the player presses the jump key
 		if (charController.isGrounded) {
-			//We want to keep this down, but it causes sticky jumps
-			//vertSpeed = 0;
+			gravity = -7f;
 			if (Input.GetButtonDown ("Jump")) {
 				vertSpeed = jumpSpeed;
-			}
+			} 
 		}
-
-		//Controls speed of the player as well as gravity
-		float deltaX = Input.GetAxis ("Horizontal") * 0.7f * acceleration;
-		float deltaZ = Input.GetAxis ("Vertical") * acceleration;
-		Vector3 movement = new Vector3 (deltaX, vertSpeed, deltaZ);
+		float deltaX = Input.GetAxis("Horizontal") * acceleration;
+		float deltaZ = Input.GetAxis("Vertical") * acceleration;
+		Vector3 movement = new Vector3(deltaX, vertSpeed, deltaZ);
 		movement.x = Mathf.Clamp (movement.x, deltaX, deltaX);
 		movement.z = Mathf.Clamp (movement.z, deltaZ, deltaZ);
 //		movement = deltaX.ClampMagnitude(movement, acceleration);
 		movement *= Time.deltaTime;
-		movement = transform.TransformDirection (movement);
-		charController.Move (movement);
+		movement = transform.TransformDirection(movement);
+		charController.Move(movement);
 		if (acceleration < maxSpeed) {
 			acceleration += .2f;
 		}
 
-		//Skate-like movement, slows player if they try to switch directions quickly
 		if (Input.GetAxis ("Horizontal") == 0 && Input.GetAxis ("Vertical") == 0 && acceleration > .6f) {
-			acceleration -= 5f;
+				acceleration -= .6f;
+			
+		}
+		if (vertSpeed > -36f) {
+			vertSpeed += gravity;
+			gravity = gravity * 1.02f;
 		}
 
-		//Causes "gravity" when jumping
-		if(!charController.isGrounded){
-			if (vertSpeed > -26f) {
-				vertSpeed += gravity;
-			} 
-		}
 	}
 }
